91c8833483584df41717a4121c726edd2fbf55a6
pOsdas <ratmir1099@mail.ru>
2025-03-31 23:12:52 +0300

    added counting of elements in the photo

--- main.py
+++ main.py
@@ -1,26 +1,49 @@
+from collections import Counter
 from ultralytics import YOLO
 import cv2
 
 
-def detect_objects(image_path, output_path):
-    # Загружаем предобученную модель YOLOv8 для сегментации.
-    model = YOLO('model/yolov8n-seg.pt')
+class ObjectDetector:
+    def __init__(self):
+        self.image_path = None
+        self.output_path = None
 
-    # Выполняем предсказание на изображении.
-    results = model(image_path)
+    @staticmethod
+    def detect_objects(image_path, output_path):
+        # Загружаем пред-обученную модель YOLOv8 для сегментации
+        # Модель из: https://github.com/matterport/Mask_RCNN
+        model = YOLO('model/yolov8n-seg.pt')
 
-    # Получаем аннотированное изображение из первого результата.
-    annotated_image = results[0].plot()
+        # Выполняем предсказание на изображении
+        results = model(image_path)
 
-    # Сохраняем результат.
-    cv2.imwrite(output_path, annotated_image)
-    print(f"Результат сохранён в {output_path}")
+        # Получаем аннотированное изображение из первого результата
+        annotated_image = results[0].plot()
+
+        # Сохраняем результат
+        cv2.imwrite(output_path, annotated_image)
+        print(f"Результат сохранён в {output_path}")
+
+        # Подсчет объектов
+        boxes = results[0].boxes
+        if boxes is not None and len(boxes.cls) > 0:
+            class_ids = boxes.cls.cpu().numpy()
+            class_names = [model.names[int(cls_id)] for cls_id in class_ids]
+
+            counts = Counter(class_names)
+
+            print("Обнаруженные объекты:")
+            for class_name, cnt in counts.items():
+                print(f"{class_name}: {cnt}")
+        else:
+            print("Объекты не обнаружены")
 
 
 def main():
     image_path = "test_input/image1.jpg"  # Путь к входному изображению
-    output_path = "output/output.jpg"  # Путь для сохранения результата
-    detect_objects(image_path, output_path)
+    output_path = "output/output1.jpg"  # Путь для сохранения результата
+    object_detector = ObjectDetector()
+    object_detector.detect_objects(image_path, output_path)
 
 
 if __name__ == "__main__":
